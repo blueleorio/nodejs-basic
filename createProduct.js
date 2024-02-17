b962445dd6ca2380f37ba5882837659c54009a16
BeluKotu <vietthanh456@gmail.com>
2024-02-17 23:11:08 +0700

    Refactor createProduct function to use Set and filter for unique product names

--- createProduct.js
+++ createProduct.js
@@ -3,13 +3,18 @@ const csv = require("csvtojson");
 
 const createProduct = async () => {
   let newData = await csv().fromFile("products.csv");
+  newData = new Set(newData.map((product) => product.product_name));
+  newData = Array.from(newData);
+  newData = newData
+    .map((product) => {
+      return { name: product };
+    })
+    .filter((product) => product.name);
+
   let data = JSON.parse(fs.readFileSync("db.json"));
-  newData = newData.map((product) => {
-    return { name: product.product_name };
-  });
-  //   console.log(newData);
+
   data.products = newData;
   fs.writeFileSync("db.json", JSON.stringify(data));
-  console.log("done");
+  console.log(newData);
 };
 createProduct();
